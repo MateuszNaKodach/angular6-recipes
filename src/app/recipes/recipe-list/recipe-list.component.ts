a2d064e8a036aa28cf74c13cd810b57471e796c1
mnowak <mateusz.nowak@intenso.pl>
2018-07-05 12:19:12 +0200

    Add recipes details and adding ingredients to shopping list.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Recipe} from '../recipe.model';
 
 @Component({
@@ -15,21 +15,23 @@ export class RecipeListComponent implements OnInit {
       'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0j8uVnrlJtD1gVey3RmCWFayMWKPueSq9OKQxLBE7tRvy-Ph5'
     ),
     new Recipe(
-      'A test recipe 1',
-      'This is simple a test number 1',
+      'A test recipe 2',
+      'This is simple a test number 2',
       'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0j8uVnrlJtD1gVey3RmCWFayMWKPueSq9OKQxLBE7tRvy-Ph5'
     ),
     new Recipe(
-      'A test recipe 1',
-      'This is simple a test number 1',
+      'A test recipe 3',
+      'This is simple a test number 3',
       'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0j8uVnrlJtD1gVey3RmCWFayMWKPueSq9OKQxLBE7tRvy-Ph5'
     ),
     new Recipe(
-      'A test recipe 1',
-      'This is simple a test number 1',
+      'A test recipe 4',
+      'This is simple a test number 4',
       'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0j8uVnrlJtD1gVey3RmCWFayMWKPueSq9OKQxLBE7tRvy-Ph5'
     )
   ];
+
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
   constructor() {
   }
@@ -37,4 +39,7 @@ export class RecipeListComponent implements OnInit {
   ngOnInit() {
   }
 
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
