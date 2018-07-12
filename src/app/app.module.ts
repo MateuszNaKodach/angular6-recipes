aafe1abdc4fce70780a1191b72f3da7ffae7e02a
mnowak <mateusz.nowak@intenso.pl>
2018-07-12 15:30:44 +0200

    Add basic AppRoutingModule.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,10 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import {FormsModule} from '@angular/forms';
 import { DropdownDirective } from './shared/dropdown.directive';
+import {Route} from '@angular/router';
+import {AppRoutingModule} from './app-routing.module';
+import { RecipeStartComponent } from './recipres/recipe-start/recipe-start.component';
+
 
 @NgModule({
   declarations: [
@@ -22,11 +26,13 @@ import { DropdownDirective } from './shared/dropdown.directive';
     RecipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
-    DropdownDirective
+    DropdownDirective,
+    RecipeStartComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
