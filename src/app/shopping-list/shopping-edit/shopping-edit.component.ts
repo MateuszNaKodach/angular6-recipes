dd00a71c2e9e2fd336d0ee3d6391b29ce81ec4e5
mnowak <mateusz.nowak@intenso.pl>
2018-08-02 22:31:46 +0200

    Add resetting ingredient form after submit.

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -35,14 +35,16 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
       );
   }
 
-  onAddItem(form: NgForm) {
+  onSubmit(form: NgForm) {
     const value = form.value;
     const newIngredient = new Ingredient(value.name, value.amount);
     if (this.editMode) {
       this.shoppingListService.updateIngredient(this.editedItemIndex, newIngredient);
-      return;
+    } else {
+      this.shoppingListService.addIngredient(newIngredient);
     }
-    this.shoppingListService.addIngredient(newIngredient);
+    this.editMode = false;
+    form.reset();
   }
 
   ngOnDestroy() {
