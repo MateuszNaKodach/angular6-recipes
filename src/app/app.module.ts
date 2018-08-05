c6bc043b446af652ce0e47ce9ff3f1150c752d65
mnowak <mateusz.nowak@intenso.pl>
2018-08-05 12:03:26 +0200

    Add lazy loading for recipes module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,25 +9,23 @@ import {HttpModule} from '@angular/http';
 import {ShoppingListService} from './shopping-list/shopping-list.service';
 import {RecipeService} from './recipes/recipe.service';
 import {DataStorageService} from './shared/data-storage.service';
-import {SignupComponent} from './auth/signup/signup.component';
-import {SigninComponent} from './auth/signin/signin.component';
 import {AuthService} from './auth/auth.service';
 import {AuthGuard} from './auth/auth-guard.service';
-import {RecipesModule} from './recipes/recipes.module';
 import {SharedModule} from './shared/shared.module';
 import {ShoppingListModule} from './shopping-list/shopping-list.module';
 import {AuthModule} from './auth/auth.module';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    RecipesModule,
     AppRoutingModule,
     HttpModule,
     SharedModule,
--- src/app/recipes/recipes-routing.module.ts
+++ src/app/recipes/recipes-routing.module.ts
@@ -8,7 +8,8 @@ import {RecipeEditComponent} from './recipe-edit/recipe-edit.component';
 
 const recipesRoutes: Routes = [
   {
-    path: 'recipes', component: RecipesComponent, children: [
+    // path: 'recipes', component: RecipesComponent, children: [
+    path: '', component: RecipesComponent, children: [ //Changed cause of lazy loading
       {path: '', component: RecipeStartComponent},
       {path: 'new', component: RecipeEditComponent, canActivate: [AuthGuard]},
       {path: ':id', component: RecipeDetailComponent},
@@ -23,4 +24,18 @@ const recipesRoutes: Routes = [
   ],
   exports: [RouterModule]
 })
-export class RecipesRoutingModule {}
+export class RecipesRoutingModule {
+}
+
+/*
+https://www.udemy.com/the-complete-guide-to-angular-2/learn/v4/t/lecture/6656742?start=0
+What if you want to use route protection (canActivate  to be precise) on lazily loaded routes?
+
+You can add canActivate to the lazy loaded routes but that of course means, that you might load code which in the end can't get accessed anyways. It would be better to check that BEFORE loading the code.
+
+You can enforce this behavior by adding the canLoad  guard to the route which points to the lazily loaded module:
+
+{ path: 'recipes', loadChildren: './recipes/recipes.module#RecipesModule', canLoad: [AuthGuard] }
+
+In this example, the AuthGuard  should implement the CanLoad interface.
+ */
